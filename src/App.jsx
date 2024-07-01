ce4920a330b2817b0c7867ac872de7e195f7cb1d
Andres Camilo Rios <acamilorm4556@gmail.com>
2024-07-01 02:28:48 -0500

    styles modified 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import Chat from './components/Chat/Chat';
 import { NavBar } from './components/NavBar/NavBar';
@@ -6,19 +6,17 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Login } from './components/LogIn/Login';
 
 function App() {
-
   return (
     <div className='app'>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/chat" element={<Chat/>} />
-        </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
 
 //Agrega () en la funcion del button para que lea cada movimiento
